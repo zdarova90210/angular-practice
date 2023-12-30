9f3eb31b09fba406dd7697f8182f393a501f2568
Maksim Mirbabaev <maksim.mirbabaev@avotech.uz>
2023-12-31 00:36:52 +0600

    Add navigation bar and new pages for Directives and Welcome
    Added new pages for Directives and Welcome, along with their corresponding TypeScript, HTML, and SCSS files. Created a new header component with navigation bar to guide user between Home, Directives, and Welcome pages.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { WelcomeComponent } from "./pages/welcome/welcome.component";
+import { DirectivesComponent } from "./pages/directives/directives.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'directives', component: DirectivesComponent},
+  {path: '', component: WelcomeComponent},
+  {path: '**', redirectTo: ''}
+];
---
+++ src/app/pages/directives/directives.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-directives',
+  standalone: true,
+  imports: [],
+  templateUrl: './directives.component.html',
+  styleUrls: ['./directives.component.scss']
+})
+export class DirectivesComponent {
+
+}
